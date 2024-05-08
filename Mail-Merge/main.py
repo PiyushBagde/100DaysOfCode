02a4f1f01fa43312b0c041b7981f34ccf8dbba0d
Piyush Bagde <100503136+PiyushBagde@users.noreply.github.com>
2024-05-09 01:46:01 +0530

    Added main.py

---
+++ Mail-Merge/main.py
@@ -0,0 +1,12 @@
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as file:
+    names = file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as file:
+    txt = file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = txt.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/letter_for_{stripped_name}.txt", mode='w') as letter:
+            letter.write(new_letter)
