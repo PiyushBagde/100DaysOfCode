59926a01085f9bbc898c114fb939cc1a353d0ac3
Piyush Bagde <piyushbagde002@gmail.com>
2024-05-09 01:53:13 +0530

    made folder adjustment and modified file path

--- Mail-Merge/main.py
+++ Mail-Merge/main.py
@@ -8,5 +8,5 @@ with open("./Input/Letters/starting_letter.txt") as file:
     for name in names:
         stripped_name = name.strip()
         new_letter = txt.replace(PLACEHOLDER, stripped_name)
-        with open(f"./Output/letter_for_{stripped_name}.txt", mode='w') as letter:
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode='w') as letter:
             letter.write(new_letter)
