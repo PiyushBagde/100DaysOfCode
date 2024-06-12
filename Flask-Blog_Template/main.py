989acc775384112163a5eab495388af1ed84ae1b
Piyush Bagde <piyushbagde002@gmail.com>
2024-06-13 00:40:41 +0530

    Revert "added other frames"
    This reverts commit 40fbcf28e6c7a09e5dbfc9e6e26de66bc6af96c5.

--- Flask-Blog_Template/main.py
+++ Flask-Blog_Template/main.py
@@ -1,14 +1,11 @@
 import requests
 from flask import Flask, render_template
-import os
 
 from post import Post
 
 app = Flask(__name__)
 
-post_endpoint = os.getenv('POSTS')
-
-posts = requests.get(post_endpoint).json()
+response = requests.get('https://api.npoint.io/c790b4d5cab58020d391').json()
 
 post_obj = []
 for post in response:
